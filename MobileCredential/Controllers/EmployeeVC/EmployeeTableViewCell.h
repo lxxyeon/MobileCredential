39722394cf7a032a8331711461e47c7994e0be1f
lxxyeon <lxxyeon@gmail.com>
2021-12-21 23:54:11 +0900

    Cell 추가

---
+++ MobileCredential/Controllers/EmployeeVC/EmployeeTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  EmployeeTableViewCell.h
+//  MobileCredential
+//
+//  Created by leeyeon2 on 2021/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EmployeeTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END

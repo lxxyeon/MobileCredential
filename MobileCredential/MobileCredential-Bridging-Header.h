b36a0ee4849ce0c6b39734a962bcaed48b860668
lxxyeon <lxxyeon@gmail.com>
2022-01-26 22:49:34 +0900

    Bridging-header 문제 해결

--- MobileCredential/MobileCredential-Bridging-Header.h
+++ MobileCredential/MobileCredential-Bridging-Header.h
@@ -1,7 +1,7 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-##ifndef MobileCredential-Bridging-Header.h
+#ifndef MobileCredential-Bridging-Header.h
 #define MobileCredential-Bridging-Header.h
 
 #import "NFCManager.h"
